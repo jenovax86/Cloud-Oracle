03dffb27073c06f9385a9849d5e9821e483bafdb
jenovax86 <shruzvn@gmail.com>
2025-09-19 11:08:03 +0330

    fix: crawler code structure and code base

--- src/crawler.py
+++ src/crawler.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
-from calendar import monthrange
+import numpy as np
 import pandas as pd
-import os
 
 MONTHS = {
     "january": 1,
@@ -22,82 +23,124 @@ MONTHS = {
     "december": 12,
 }
 COUNTRY = "iran"
-YEAR = 2024
+CITY = "tehran"
 WEBDRIVER_TIMEOUT = 20
 
 
-def extract_month_days(days, month_number):
-    valid_days = []
-    _, num_of_days = monthrange(YEAR, month_number)
-    for day in days:
-        date_value = day.find_element(By.CLASS_NAME, "date").text.strip()
-        if not date_value.isdigit():
-            continue
-
-        date_number = int(date_value)
-        if 1 <= date_number <= num_of_days:
-            date = datetime(YEAR, month_number, date_number)
-            valid_days.append((date, day))
-
-    return valid_days
+def previous_years():
+    current_year = datetime.now().year
+    years = list(range(current_year - 1, current_year - 6, -1))
+    years.reverse()
+    return years
 
 
-def parse_daily_temperatures(month_days, month_name):
+def extract_date(date):
+    return datetime.strptime(date.text, "%d %B %Y")
+
+
+def clean_temperature_unit(temperature):
+    temperature_value = temperature.replace("°C", "").strip()
+    if temperature_value.isdigit():
+        return int(temperature_value)
+
+
+def clean_wind_unit(wind):
+    wind_value = wind.replace("km/h", "").strip()
+    if wind_value.isdigit():
+        return int(wind_value)
+
+
+def clean_humidity_unit(humidity):
+    humidity_value = humidity.replace("%", "").strip()
+    if humidity_value.isdigit():
+        return int(humidity_value)
+
+
+def mean_rounded(array):
+    return np.round(np.mean(array)).astype(int)
+
+
+def get_weather_specifics(driver):
+    try:
+        rows = WebDriverWait(driver, WEBDRIVER_TIMEOUT).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, "//table[@id = 'wt-his']//tbody/tr")
+            )
+        )
+        temperatures, winds, humidities = [], [], []
+        for row in rows:
+            try:
+                tds = row.find_elements(By.TAG_NAME, "td")
+                temperature = clean_temperature_unit(tds[1].text)
+                wind = clean_wind_unit(tds[3].text)
+                humidity = clean_humidity_unit(tds[5].text)
+                if not None in (temperature, wind, humidity):
+                    temperatures.append(temperature)
+                    winds.append(wind)
+                    humidities.append(humidity)
+            except StaleElementReferenceException:
+                continue
+        if all([temperatures, winds, humidities]):
+            mean_temp = mean_rounded(temperatures)
+            mean_wind = mean_rounded(winds)
+            mean_humidity = mean_rounded(humidities)
+        return mean_temp, mean_wind, mean_humidity
+    except Exception as e:
+        raise ValueError(e)
+
+
+def select_days_dropdown(driver):
     weather_data = []
-    for date_num, day in month_days:
-        temp_parent_element = day.find_element(By.CLASS_NAME, "temp")
-        temp_low = (
-            temp_parent_element.find_element(By.CLASS_NAME, "low").text
-            if temp_parent_element.find_element(By.CLASS_NAME, "low")
-            else None
+    select_element = driver.find_element(By.NAME, "start")
+    select = Select(select_element)
+    for option in select.options:
+        date = extract_date(option)
+        option_date_value = option.get_attribute("value")
+        select.select_by_value(option_date_value)
+
+        WebDriverWait(driver, WEBDRIVER_TIMEOUT).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, "//table[@id='wt-his']//tbody/tr")
+            )
         )
-        temp_high = (
-            temp_parent_element.find_element(By.CLASS_NAME, "high").text
-            if temp_parent_element.find_element(By.CLASS_NAME, "high")
-            else None
-        )
+        temp, wind, humidity = get_weather_specifics(driver)
         weather_data.append(
             {
-                "month": month_name,
-                "day": date_num.strftime("%a"),
-                "date": date_num.strftime("%Y-%m-%d"),
-                "lowest_temperature": temp_low,
-                "highest_temperature": temp_high,
+                "country": COUNTRY,
+                "city": CITY,
+                "date": date,
+                "temperature": temp,
+                "wind": wind,
+                "humidity": humidity,
             }
         )
-
     return weather_data
-
-
-def data_preprocessing(df):
-    df["date"] = pd.to_datetime(df["date"], errors="coerce")
-    df = df.drop_duplicates(subset=["date"], keep="first").reset_index(drop=True)
-    df = df.sort_values("date").reset_index(drop=True)
-
-    for column in ["lowest_temperature", "highest_temperature"]:
-        df[column] = df[column].astype(str).str.replace(r"[^0-9\-]", "", regex=True)
-        df[column] = pd.to_numeric(df[column], errors="coerce").astype("Int64")
-
-    df = df.dropna(subset=["lowest_temperature", "highest_temperature"])
-    return df
 
 
 def scrape_weather_data():
     weather_scrapped_data = []
     driver = webdriver.Chrome()
-    for month_name, month_number in MONTHS.items():
-        url = f"https://www.accuweather.com/en/ru/{COUNTRY}/605458/{month_name}-weather/605458?year=2024"
-        driver.get(url)
+    years = previous_years()
+    for year in years:
+        for _, month_number in MONTHS.items():
+            driver.get(
+                f"https://www.timeanddate.com/weather/{COUNTRY}/{CITY}/historic?month={month_number}&year={year}"
+            )
 
-        days = WebDriverWait(driver, WEBDRIVER_TIMEOUT).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".monthly-daypanel"))
-        )
-
-        pd.options.display.max_rows=1000
-        month_days = extract_month_days(days, month_number)
-        result = parse_daily_temperatures(month_days, month_name)
-        weather_scrapped_data.extend(result)
-
+            WebDriverWait(driver, WEBDRIVER_TIMEOUT).until(
+                EC.presence_of_element_located((By.ID, "wt-his"))
+            )
+            WebDriverWait(driver, WEBDRIVER_TIMEOUT).until(
+                EC.presence_of_all_elements_located(
+                    (By.XPATH, "//table[@id = 'wt-his']//tbody/tr")
+                )
+            )
+            weather_scrapped_result = select_days_dropdown(driver)
+            if not weather_scrapped_data:
+                weather_scrapped_data.extend(weather_scrapped_result)
     driver.quit()
-    df = pd.DataFrame(weather_scrapped_data)
-    return data_preprocessing(df)
+    return pd.DataFrame(weather_scrapped_data)
+
+
+
+scrape_weather_data()
