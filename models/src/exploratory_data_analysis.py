c3e148c70c7a378cfe00295952b4e05176b1b495
jenovax86 <shruzvn@gmail.com>
2025-10-08 03:39:03 +0330

    fix: check_for_outliers function logic

--- models/src/exploratory_data_analysis.py
+++ models/src/exploratory_data_analysis.py
@@ -1,8 +1,10 @@
 import sqlite3
 import sys
 import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 import seaborn as sns
 
 sys.path.append(
@@ -29,27 +31,30 @@ def convert_temperatures_to_integer(df):
 
 
 def calculate_average_low_and_high_temperature_per_day(df):
-    df["date"] = pd.to_datetime(df["date"])
-    df["date"] = df["date"].dt.date
-    round(df.groupby(df["date"])[["low_temperature", "high_temperature"]].mean())
-    return df
+    df["date"] = pd.to_datetime(df["date"]).dt.date
+    daily_mean = round(
+        df.groupby(df["date"]).agg(
+            low_temperature=("low_temperature", "mean"),
+            high_temperature=("high_temperature", "mean"),
+        )
+    ).reset_index()
+    return daily_mean
+
 
 def check_for_outliers_in_temperatures_per_month(df, column):
+    df["month"] = pd.to_datetime(df["date"]).dt.month
     for month in range(1, 13):
-        temp_per_month = df[df["month"] == month][column]
-        Q1 = temp_per_month.quantile(0.25)
-        Q3 = temp_per_month.quantile(0.75)
+        temperature_per_month = df[df["month"] == month][column]
+        Q1 = temperature_per_month.quantile(0.25)
+        Q3 = temperature_per_month.quantile(0.75)
         IQR = Q3 - Q1
         lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 - 1.5 * IQR
-        outliers = temp_per_month[
-            (df[column] < lower_bound) | (df[column] > upper_bound)
+        upper_bound = Q3 + 1.5 * IQR
+        outliers = temperature_per_month[
+            (temperature_per_month < lower_bound)
+            | (temperature_per_month > upper_bound)
         ]
-        print(f"Month {month}")
-        print("Lower bound:", lower_bound)
-        print("Upper bound:", upper_bound)
-        print("Outliers found:", len(outliers))
-        print("Sample outliers:\n", outliers.head(), "\n")
+        return outliers
 
 
 def scatter_plot_of_temperatures(df):
@@ -76,11 +81,5 @@ def data_cleaning_and_preprocessing():
     df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
     df = convert_temperatures_to_integer(df)
-    check_for_outliers_in_temperatures_per_month(df, "low_temperature")
-    check_for_outliers_in_temperatures_per_month(df, "high_temperature")
-    calculate_average_low_and_high_temperature_per_day(df)
-    print(df.head())
+    df = calculate_average_low_and_high_temperature_per_day(df)
     return df
-
-
-data_cleaning_and_preprocessing()
